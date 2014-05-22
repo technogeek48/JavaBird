20529ad39917cee4d3413d6a1b9b758709a82303
technogeek48 <michael@mjstech.ca>
2014-05-22 07:28:24 -0400

    dedicated menu drawing method

--- src/me/technogeek48/JavaBird/Main/Menu.java
+++ src/me/technogeek48/JavaBird/Main/Menu.java
@@ -19,6 +19,8 @@ public class Menu extends BasicGameState{
 
     TrueTypeFont titleFont;
 
+
+
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
         Logger logger = Logger.getLogger("LogHelper");
         logger.log(Level.INFO, "&a Menu State Initialized");
@@ -26,14 +28,17 @@ public class Menu extends BasicGameState{
         titleFont = new TrueTypeFont(awtFont, false);
     }
 
-    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{ //480 177
+    private void drawMenu(Graphics g, GameContainer gc) throws SlickException{
         Image bird = new Image("resources/GreenBird.png");
-        //g.drawRect(50, 100, 60, 120);
         titleFont.drawString(568, 118, "JavaBird", Color.blue);
         int mouseX = gc.getInput().getMouseX();
         int mouseY = gc.getInput().getMouseY();
         g.drawString("X: " + mouseX + " Y: " + mouseY, 1025, 10);
         g.drawImage(bird, 480, 160);
+    }
+
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{ //480 177
+        drawMenu(g, gc);
     }
 
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
