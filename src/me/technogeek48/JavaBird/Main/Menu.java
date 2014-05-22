1a405fe4f671c827bbd460eece6230c00c7849e8
Michael-John (technogeek48) <michael.john.tech@gmail.com>
2014-05-22 16:30:11 -0400

    Code Cleanup and menu selection start

--- src/me/technogeek48/JavaBird/Main/Menu.java
+++ src/me/technogeek48/JavaBird/Main/Menu.java
@@ -1,5 +1,6 @@
 package me.technogeek48.JavaBird.Main;
 
+import com.sun.corba.se.impl.orbutil.graph.Graph;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
@@ -30,11 +31,20 @@ public class Menu extends BasicGameState{
 
     private void drawMenu(Graphics g, GameContainer gc) throws SlickException{
         Image bird = new Image("resources/GreenBird.png");
-        titleFont.drawString(568, 118, "JavaBird", Color.blue);
+        g.drawImage(bird, 480, 160);
+        titleFont.drawString(625, 145, "JavaBird", Color.blue);
+        drawMouseCoords(g,gc);
+        drawStateSelector(g,gc);
+    }
+
+    private void drawMouseCoords(Graphics g, GameContainer gc){
         int mouseX = gc.getInput().getMouseX();
         int mouseY = gc.getInput().getMouseY();
         g.drawString("X: " + mouseX + " Y: " + mouseY, 1025, 10);
-        g.drawImage(bird, 480, 160);
+    }
+
+    private void drawStateSelector(Graphics g, GameContainer gc){
+        g.drawRoundRect(432, 215, 394, 277, 5);
     }
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{ //480 177
